2e5825861faf0035bd647c60e2b455f02669ebd9
Syed Asrar Zahoor <81914950+Syed-Asrar@users.noreply.github.com>
2022-12-10 15:51:51 +0530

    intialized project

---
+++ src/main/java/com/netflix/backend/NetflixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixBackendApplication.class, args);
+	}
+
+}
